e14054e573a1e8f6602a10247e1af50e515aaf0f
Rodrigo Maciel <69148290+Rinogahr@users.noreply.github.com>
2022-06-30 21:57:25 -0300

    jhjhjl
    lklçklç

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 let taskInput = document.getElementById("addTask");
 let taskButton = document.querySelector('#inputaddTask button');
 let listTask = document.getElementById('listTask');
-let taskList = [];
-let redenTaskList = [];
+let taskList = JSON.parse(localStorage.getItem("@taskList")) || [];
 
 function verificarNovaTarefa(){
     if(!taskInput.value){
@@ -12,46 +11,55 @@ function verificarNovaTarefa(){
         taskList.push(taskInput.value);
         taskInput.value = "";
         addNovaTarefa();
+        salvarDados();
     }
 }
 
 function addNovaTarefa(){
     listTask.innerHTML = "";
-    redenTaskList = localStorage.getItem("taskList");
-    
-    taskList.map( ( todo ) => {
-        let valor = todo.replace(/ /g, "");
-        let divTaskPai = document.createElement("div");
-        divTaskPai.className = "taskConteiner";
-        divTaskPai.id = valor;
-        let position = taskList.indexOf(todo);
-        divTaskPai.innerHTML = `
-        <div>
-            <ul>
-                <li>
-                    <label for="*">${todo}</label>
-                </li>
-            </ul>
-        </div>
-        <div id="trashButton" classe="trashButton">
+
+    try {
+        taskList.map( ( todo ) => {
+            let valor = todo.replace(/ /g, "");
+            let divTaskPai = document.createElement("div");
+            divTaskPai.className = "taskConteiner";
+            divTaskPai.id = valor;
+            let position = taskList.indexOf(todo);
+            divTaskPai.innerHTML = `
             <div>
-                <button onclick="exluirTask(${position})">Esxcluir Tarefa</button>
+                <ul>
+                    <li>
+                        <label for="*">${todo}</label>
+                    </li>
+                </ul>
             </div>
-            <div>
-                <img src="./img/trash.png" alt="Example1" width="40" height="40">
+            <div id="trashButton" classe="trashButton">
+                <div>
+                    <button onclick="exluirTask(${position})">Esxcluir Tarefa</button>
+                </div>
+                <div>
+                    <img src="./img/trash.png" alt="Example1" width="40" height="40">
+                </div>
             </div>
-        </div>
-    `;
-    listTask.appendChild(divTaskPai);
-    localStorage.setItem("taskList", JSON.stringify(taskList));
-    console.log('redenTaskList :>> ', redenTaskList);
+        `;
+        listTask.appendChild(divTaskPai);
     });
+    } catch (error) {
+        console.log("Não foi possivel renderizar a lista de tarefa motivo ::>  ", error);
+    }
 
 }
+
+addNovaTarefa();
 
 function exluirTask(position){
     taskList.splice( position, 1);
     addNovaTarefa();
+    salvarDados();
+}
+
+function salvarDados(){
+    localStorage.setItem("@taskList", JSON.stringify(taskList));
 }
 
 taskButton.onclick = verificarNovaTarefa;
