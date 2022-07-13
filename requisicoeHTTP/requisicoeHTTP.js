7d578038b0391b1faa081638073e9b0d708d08f9
Rinogahr <69148290+Rinogahr@users.noreply.github.com>
2022-07-13 16:59:08 -0300

    asasas
    asasas

--- requisicoeHTTP/requisicoeHTTP.js
+++ requisicoeHTTP/requisicoeHTTP.js
@@ -19,16 +19,36 @@ function appNutricao(){
     
 }
 
-appNutricao();
 
 function renderApi(vl){
+    
+    console.log(vl);
     // "id": 1,
     // "titulo": "Refeições proteicas para fazer antes de dormir",
     // "capa": "https://sujeitoprogramador.com/nutriapp/wp-content/uploads/2017/12/Screenshot_3-2.jpg",
     // "subtitulo": "O que acontece com nossos músculos quando estamos dormindo Muitas pessoas pensam que o ganho de massa muscular ocorre dentro da academia, quando estamos treinando pesado. O inchaço do treino e pós-treino dá a impressão que estamos evoluindo, mas isto não passa de...",
     // "categoria": "Deita"
     if(vl){
-        let liElement = document.createElement("li");
+        vl.map( (item) =>{
+            console.log(item);
+            let liElement = document.createElement("li");
+            let titleElement = document.createElement("strong");
+            let imgElement = document.createElement("img");
+            let descriptionElement = document.createElement("a");
+            
+            let titleTxt = document.createTextNode(item.titulo);
+            titleElement.appendChild(titleTxt);
+            liElement.append(titleElement);
+            
+            imgElement.src = item.capa;
+            liElement.appendChild(imgElement);
+
+            let descriptionTxt = document.createTextNode(item.subtitulo);
+            descriptionElement.appendChild(descriptionTxt);
+            liElement.appendChild(descriptionElement);
+
+            listElement.appendChild(liElement);
+        })
 
     }else{
         
@@ -36,3 +56,6 @@ function renderApi(vl){
     }
 
 };
+
+
+appNutricao();
